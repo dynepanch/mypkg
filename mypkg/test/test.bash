59ba1038701c93efffa392d2ab059e9b64ce7268
KenInaba <jiandaoye293@gmail.com>
2022-12-01 11:55:33 +0900

    latest

--- mypkg/test/test.bash
+++ mypkg/test/test.bash
@@ -3,10 +3,10 @@
 dir=~
 [ "$1" != "" ] && dir="$1"
 
-cd $dir/ros2_ws3/ros2_ws
+cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 5 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
-grep 'Listen: 5'
+grep 'Listen: 10'
