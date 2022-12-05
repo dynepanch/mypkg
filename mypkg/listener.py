41285b169cae12d64b09e501cade3a971dd32628
KenInaba <jiandaoye293@gmail.com>
2022-12-06 08:50:30 +0900

    listner_lesson11

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -2,12 +2,14 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
 
-def cd(msg):
-        global node
-        node.get_logger().info("Listen: %d" % msg.data)
-
-rclpy.init()
-node = Node("listner")
-pub = node.create_subscription(Int16,"countup",cd,10)
-rclpy.spin(node)
+class Listner():
+        def __init__(self):
+            self.pub = node.create_subscription(Int16,"countup",self.cd,10)
+    def cd(msg):
+            global node
+            node.get_logger().info("Listen: %d" % msg.data)
+def main():
+    rclpy.init()
+    node = Node("listner")
+    rclpy.spin(node)
 
